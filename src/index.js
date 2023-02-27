b9816cf37617524ec0dda0127abecaedc451a342
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-02-27 21:53:26 +0800

    Refactor to a module pattern

--- src/index.js
+++ src/index.js
@@ -1,41 +1,63 @@
 // This module is in charge of connecting the tasks to the UI
 console.log("--index.js is running.");
 
-import { todo, project } from "./tasks.js";
-import { displayProject } from "./todo.js";
+import { todo, project } from "./projAndTodoClass.js";
 
+// ------------- Body Container (All)
 const content = document.getElementById("content");
-// Project Section
-    const projSection = document.createElement("div");
-    projSection.classList.add("projSection");
-    content.appendChild(projSection);
 
-    // Create button that makes project objects
-    const newProjBtn = document.createElement("button");
-    newProjBtn.innerText = "New Project";
-    newProjBtn.classList.add("newProjBtn");
-    newProjBtn.addEventListener("click", () => {
-        // Create project object and it's corresponding display.
-        const project1 = new project("New Project", "project description...");
-        const projBox = document.createElement("div");
-        const title = document.createElement("h5");
-        const desc = document.createElement("h6");
-        title.textContent = project1.title;
-        desc.textContent = project1.desc;
-        projBox.appendChild(title);
-        projBox.appendChild(desc);
-        // Append project to the section
-        projSection.appendChild(projBox);
+// ------------- Project Section (Left)
+const projSection = document.createElement("div");
+projSection.classList.add("projSection");
+content.appendChild(projSection);
+    // Projects array, if array is not empty, display first project. 
+const projects = [];
+if(projects.length !== 0) {
+    console.log("Display first project");
+} 
+    // Create projects button, show form on click
+const newProjBtn = document.createElement("button");
+newProjBtn.classList.add("newProjBtn");
+newProjBtn.innerText = "New Project";
+newProjBtn.addEventListener("click", createProjectForm) // #### Create form
+projSection.appendChild(newProjBtn);
+    // Projects button, for each project on the projects list, display contents on click
+    // Add logic to display each project from the list on the projects section.
 
-        displayProject(project1);
-    });
-    projSection.appendChild(newProjBtn);
+
+// ------------- Todo Section (Right)
+const todoSection = document.createElement("div");
+todoSection.classList.add("todoSection");
+content.appendChild(todoSection);
+
+
+
+
+
+
+
+    // // Create button that makes project objects
+    // const newProjBtn = document.createElement("button");
+    // newProjBtn.innerText = "New Project";
+    // newProjBtn.classList.add("newProjBtn");
+    // newProjBtn.addEventListener("click", () => {
+    //     // Create project object and it's corresponding display.
+    //     const project1 = new project("New Project", "project description...");
+    //     const projBox = document.createElement("div");
+    //     const title = document.createElement("h5");
+    //     const desc = document.createElement("h6");
+    //     title.textContent = project1.title;
+    //     desc.textContent = project1.desc;
+    //     projBox.appendChild(title);
+    //     projBox.appendChild(desc);
+    //     // Append project to the section
+    //     projSection.appendChild(projBox);
+
+    //     displayProject(project1);
+    // });
+    // projSection.appendChild(newProjBtn);
 
     // ######## TO DO: Show a form on the button click and get the project object's values from there
     // ######## OR ALTERNATIVELY: Implement the same basic logic for the todolist section
     
-// Todo Section
-export const todoSection = document.createElement("div");
-todoSection.classList.add("todoSection");
-content.appendChild(todoSection);
 
---
+++ src/projectModule.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export function displayProject() {
+//     const project1 = new project("New Project", "project description...");
+//     const projBox = document.createElement("div");
+//     const title = document.createElement("h5");
+//     const desc = document.createElement("h6");
+//     title.textContent = project1.title;
+//     desc.textContent = project1.desc;
+//     projBox.appendChild(title);
+//     projBox.appendChild(desc);
+//     // Append project to the section
+//     projSection.appendChild(projBox);
+// }
